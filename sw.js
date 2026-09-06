309836566bf900db2adaaf0e1441d967bf77f5ef
vishalrajpurohit98 <vishal.purohit.1255@Gmail.com>
2026-09-06 11:17:56 +0530

    Fix Excel import reader loading

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'habits-v2-77-v21';
+const CACHE = 'habits-v2-78-v22';
 const FILES = ['.', 'index.html', 'unbounded.ttf', 'fraunces.ttf', 'spacegrotesk.ttf', 'bricolage.ttf', 'xlsx.min.js', 'firebase-app-compat.js', 'firebase-auth-compat.js', 'manifest.json', 'icon-192.png', 'icon-512.png', 'favicon-32.png', 'apple-touch-icon.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
